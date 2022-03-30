d3b08b24b6d7bb251122923fb37b8915cc644fda
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-03-29 20:11:43 -0500

    add product dto

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,7 +1,7 @@
 export class CreateProductDTO {
-  readonly name: string
-  readonly description: string
-  readonly imageURL: string
-  readonly price: number
-  readonly createdAt: Date
+  readonly name: string;
+  readonly description: string;
+  readonly imageURL: string;
+  readonly price: number;
+  readonly createdAt: Date;
 }
