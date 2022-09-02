8579392e02d6015c7acc5404bf3b23d975cabdd2
Brayan Gómez <57688651+brayangomez22@users.noreply.github.com>
2022-09-01 22:08:30 -0500

    first commit

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateProductDTO } from './dto/product.dto';
 
 @Controller('product')
 export class ProductController {
   @Post('create')
-  createProduct(@Res() res) {
+  createProduct(@Res() res, @Body() ceateProductDTO: CreateProductDTO) {
     return res.status(HttpStatus.OK).json({
       message: 'received',
+      ceateProductDTO,
     });
   }
 }
